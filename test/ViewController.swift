b781318670c9b7d55b08e59f6e290ec58053a84f
David Shustin <DAVID@David-Shustins-MacBook-Pro.local>
2016-10-05 20:11:29 -0400

    r

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var status: UILabel!
     @IBOutlet weak var result: UILabel!
     @IBOutlet weak var input: UITextField!
     @IBOutlet weak var userAction: UIButton!
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         userAction.addTarget(self, action: #selector(ViewController.onPress), for: .touchUpInside)
     }
     func isPrime(_ num: Int) -> Bool {
+        if num == 3 {
+            return true
+        }
         for var i in 2...Int(floor(Double(sqrt(Float(num))))) {
             if (num % i == 0) {
                 return false
@@ -28,8 +31,11 @@ class ViewController: UIViewController {
         return true
     }
     func onPress () {
+        status.text = "Status: Calculating..."
         toTest = Int(input.text!)
-        result.text = toTest == nil ? "Please enter valid input." : (isPrime(toTest!) ? "This number is prime!" : "This number is composite!")
+        result.text = toTest == nil || toTest! < 0 ? "Please enter valid input." : (toTest! < 2 ? "Neither prime nor composite." : (isPrime(toTest!) ? "This number is prime!" : "This number is composite!"))
+        status.text = "Status: Done!"
+
         //print(Int("test"))
         
     }
